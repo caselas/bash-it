587af7baca6a3ac1f45dbd4d36edd8d9923a8a03
Chris Caselas <dev@chriscaselas.me>
2014-10-12 20:10:02 -0700

    Commit my custom aliases.

---
+++ aliases/custom.aliases.bash
@@ -0,0 +1,35 @@
+# Bash history size
+HISTFILESIZE=500000
+
+# Bundler
+alias be="bundle exec"
+alias rc="bundle exec rails console"
+alias rs="bundle exec rails server"
+alias rk="bundle exec rake"
+alias spec="bundle exec rspec --color --format d"
+alias fs="foreman start"
+
+# Git
+alias gut="git"
+alias got="git"
+alias gst="git status"
+alias prune="git remote prune origin && git branch --merged | grep -v \"\*\" | xargs -n 1 git branch -d"
+alias staged="git diff --staged"
+alias stats="git diff --stat"
+alias master="git checkout master"
+alias pull="master && git pull && prune"
+alias branch="git checkout -b"
+alias checkout="git checkout"
+
+# Applications
+alias vim="open -a MacVim"
+alias mate="open -a TextMate"
+alias lime="open -a Sublime\ Text"
+alias atom="open -a Atom"
+alias chrome="open -a Google\ Chrome"
+
+# Utilities
+alias fuckoffds="find . -name ".DS_Store" -delete"
+
+# Docker
+export DOCKER_HOST=tcp://localhost:4243
